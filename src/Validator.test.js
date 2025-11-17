cf7119ff09aa71b44e04ae4984646d4869814e68
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-17 15:47:21 +0100

    REFACTORING

--- src/Validator.test.js
+++ src/Validator.test.js
@@ -12,21 +12,21 @@ describe('Validator class test suite', () => {
   })
 
   describe('checkTitle() test suite', () => {
-    test('Should return false when title is empty string', () => {
-      const validator = new Validator()
+    let validator
 
+    beforeEach(() => {
+      validator = new Validator()
+    })
+
+    test('Should return false when title is empty string', () => {
       expect(validator.isTitleCorrect('')).toBe(false)
     })
 
     test('Should return true when title has content', () => {
-      const validator = new Validator()
-
       expect(validator.isTitleCorrect('content')).toBe(true)
     })
 
     test('Should return false when title is longer than 50 characters', () => {
-      const validator = new Validator()
-
       expect(
         validator.isTitleCorrect(
           'Structuring test can be done trough `triple A`. Arrange'
