fe6323f36eb15eee5839f68834630bec084a7ced
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-17 16:49:04 +0100

    RED

---
+++ src/StorageManager.test.js
@@ -0,0 +1,13 @@
+import { StorageManager } from './StorageManager'
+
+describe('StorageManager class test suite', () => {
+  test('StorageManager class should exist', () => {
+    // Arrange
+    const storageManager = new StorageManager()
+
+    // Act
+
+    // Assert
+    expect(storageManager).toBeDefined()
+  })
+})
