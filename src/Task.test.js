0a32a0396a8d69aac69cb1834bf3829ea4712195
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-17 15:59:03 +0100

    RED

---
+++ src/Task.test.js
@@ -0,0 +1,13 @@
+import { Task } from './Task'
+
+describe('Task class test suite', () => {
+  test('Task class should exist', () => {
+    // Arrange
+    const Task = new Task()
+
+    // Act
+
+    // Assert
+    expect(validator).toBeDefined()
+  })
+})
