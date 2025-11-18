1aa63244cbc6bd3374e32d754dea3136766a714f
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-18 13:47:20 +0100

    GREEN

--- src/StorageManager.js
+++ src/StorageManager.js
@@ -1,1 +1,16 @@
-export class StorageManager {}
+export class StorageManager {
+  #fileSaveInterface
+
+  constructor(fileSaveInterface) {
+    this.#fileSaveInterface = fileSaveInterface
+  }
+
+  save(task) {
+    const jsonFormated = this.#formatToJson(task)
+    this.#fileSaveInterface.writeFile('./tasks.json', jsonFormated)
+  }
+
+  #formatToJson(input) {
+    return JSON.stringify(input, null, 2)
+  }
+}
