2edcac2c2501f2aa145072748ecf4d7df4496e5b
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-18 13:36:55 +0100

    RED

--- src/StorageManager.test.js
+++ src/StorageManager.test.js
@@ -1,4 +1,6 @@
 import { StorageManager } from './StorageManager'
+import { jest } from '@jest/globals'
+import { Task } from './Task'
 
 describe('StorageManager class test suite', () => {
   test('StorageManager class should exist', () => {
@@ -9,5 +11,20 @@ describe('StorageManager class test suite', () => {
 
     // Assert
     expect(storageManager).toBeDefined()
+  })
+
+  test('Should save tasks to a JSON file', async () => {
+    const fsMock = {
+      writeFile: jest.fn().mockResolvedValue()
+    }
+    const storageManager = new StorageManager(fsMock)
+    const task = new Task()
+
+    await storageManager.save(task)
+
+    expect(fsMock.writeFile).toHaveBeenCalledWith(
+      './tasks.json',
+      JSON.stringify(task, null, 2)
+    )
   })
 })
