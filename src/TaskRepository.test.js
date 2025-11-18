3559e91e7e90c476daa336bbbdf8dad98c6ae6c9
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-18 21:47:30 +0100

    RED

--- src/TaskRepository.test.js
+++ src/TaskRepository.test.js
@@ -27,4 +27,24 @@ describe('TaskRepository class test suite', () => {
     // Assert
     expect(storageManagerMock.save).toHaveBeenCalledWith(task)
   })
+
+  test('Should be able to retrieve all stored tasks', () => {
+    // Arrange
+    const task1 = new Task()
+    const task2 = new Task()
+    const task3 = new Task()
+    const storageManagerMock = {
+      save: jest.fn(),
+      load: jest.fn().mockRejectedValue([task1, task2, task3])
+    }
+    const taskRepository = new TaskRepository(storageManagerMock)
+
+    // Act
+    taskRepository.add(task1)
+    taskRepository.add(task2)
+    taskRepository.add(task3)
+
+    // Assert
+    expect(taskRepository.getAllTasks()).toEqual([task1, task2, task3])
+  })
 })
