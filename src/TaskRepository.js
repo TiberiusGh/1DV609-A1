a8b7d996056d977d184031bb8084db7d606650f9
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-18 22:06:20 +0100

    GREEN

--- src/TaskRepository.js
+++ src/TaskRepository.js
@@ -2,11 +2,18 @@ import { StorageManager } from './StorageManager'
 
 export class TaskRepository {
   #storageManager
+  #tasks = []
+
   constructor(storageManager = new StorageManager()) {
     this.#storageManager = storageManager
   }
 
   add(newTask) {
     this.#storageManager.save(newTask)
+    this.#tasks.push(newTask)
+  }
+
+  getAllTasks() {
+    return this.#tasks
   }
 }
