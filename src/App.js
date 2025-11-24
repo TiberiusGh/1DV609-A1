7f656fa6f8b1405af54bcf9eef790370c69fa6ba
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-24 20:40:26 +0100

    GREEN

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-export class App {}
+export class App {
+  #outputLogger
+
+  constructor(output) {
+    this.#outputLogger = output || console
+  }
+
+  async start() {
+    this.#outputLogger.log('1. List all saved tasks')
+    this.#outputLogger.log('2. Add new task')
+    this.#outputLogger.log('3. Exit')
+    this.#outputLogger.log('Your input:')
+  }
+}
