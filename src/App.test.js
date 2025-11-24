b2ffefbb5dcbb404c63a96c452402b764c53c59b
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-24 20:37:52 +0100

    RED

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { App } from './App'
-import { TaskRepository } from './TaskRepository'
+import { jest } from '@jest/globals'
 
 describe('App class test suite', () => {
   test('App class should exist', () => {
@@ -10,5 +10,30 @@ describe('App class test suite', () => {
 
     // Assert
     expect(app).toBeDefined()
+  })
+
+  test('Should display menu options when App is instantiated', async () => {
+    // Arrange
+    const outputMock = {
+      log: jest.fn()
+    }
+    const app = new App(outputMock)
+
+    // Act
+    await app.start()
+
+    // Assert
+    expect(outputMock.log).toHaveBeenCalledWith(
+      expect.stringContaining('1. List all saved tasks')
+    )
+    expect(outputMock.log).toHaveBeenCalledWith(
+      expect.stringContaining('2. Add new task')
+    )
+    expect(outputMock.log).toHaveBeenCalledWith(
+      expect.stringContaining('3. Exit')
+    )
+    expect(outputMock.log).toHaveBeenCalledWith(
+      expect.stringContaining('Your input:')
+    )
   })
 })
