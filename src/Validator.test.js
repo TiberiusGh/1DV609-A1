04e03dcf35c4dafebff97fa5c24618bfe900688e
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-25 17:05:25 +0100

    REFACTORING

--- src/Validator.test.js
+++ src/Validator.test.js
@@ -9,7 +9,6 @@ describe('Validator class test suite', () => {
   })
 
   test('Validator class should exist', () => {
-    // Arrange
     // Act
 
     // Assert
@@ -18,14 +17,23 @@ describe('Validator class test suite', () => {
 
   describe('checkTitle() test suite', () => {
     test('Should return false when title is empty string', () => {
+      // Act
+
+      // Assert
       expect(validator.isTitleCorrect('')).toBe(false)
     })
 
     test('Should return true when title has content', () => {
+      // Act
+
+      // Assert
       expect(validator.isTitleCorrect('content')).toBe(true)
     })
 
     test('Should return false when title is longer than 50 characters', () => {
+      // Act
+
+      // Assert
       expect(
         validator.isTitleCorrect(
           'Structuring test can be done trough `triple A`. Arrange'
@@ -34,19 +42,27 @@ describe('Validator class test suite', () => {
     })
 
     test('Should return false when title is null', () => {
+      // Act
+
+      // Assert
       expect(validator.isTitleCorrect(null)).toBe(false)
     })
 
     describe('checkPriority() test suite', () => {
       test('Should return true when the priority is "low", "medium" or "high"', () => {
+        // Act
+
+        // Assert
         expect(validator.checkPriority('low')).toBe(true)
         expect(validator.checkPriority('medium')).toBe(true)
         expect(validator.checkPriority('high')).toBe(true)
       })
 
       test('Should return false when the priority is other than "low", "medium" or "high"', () => {
+        // Act
         const priority = 'wrong priority'
 
+        // Assert
         expect(validator.checkPriority(priority)).toBe(false)
       })
     })
@@ -54,6 +70,9 @@ describe('Validator class test suite', () => {
 
   describe('validateMenuChoice() test suite', () => {
     test('returns false when wrong menu option is called', () => {
+      // Act
+
+      // Assert
       expect(validator.validateMenuChoice('0')).toBe(false)
       expect(validator.validateMenuChoice('4')).toBe(false)
       expect(validator.validateMenuChoice('a')).toBe(false)
