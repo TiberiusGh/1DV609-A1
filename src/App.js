631d3a4a26bf95d9700982308cb4337fad6c0ac4
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-11-25 13:32:31 +0100

    GREEN

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import * as readline from 'node:readline/promises'
+
 export class App {
   #outputLogger
+  #input
 
-  constructor(output) {
+  constructor(output, input) {
     this.#outputLogger = output || console
+    this.#input =
+      input ||
+      readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      })
   }
 
   async start() {
@@ -10,5 +19,8 @@ export class App {
     this.#outputLogger.log('2. Add new task')
     this.#outputLogger.log('3. Exit')
     this.#outputLogger.log('Your input:')
+
+    const input = await this.#input.question('Your input: ')
+    console.log(input)
   }
 }
