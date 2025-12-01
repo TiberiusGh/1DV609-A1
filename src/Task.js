467d4d50cb81f3bf1f866155ed91324e7fd34d01
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-12-01 14:27:44 +0100

    GREEN

--- src/Task.js
+++ src/Task.js
@@ -1,10 +1,13 @@
+import { Validator } from './Validator.js'
+
 export class Task {
   #title
   #priority
   #completed
+  #validator = new Validator()
 
   constructor({ title, priority, completed }) {
-    this.#title = title
+    this.#title = this.#validateTitle(title)
     this.#priority = priority
     this.#completed = completed
   }
@@ -34,6 +37,16 @@ export class Task {
       title: this.getTitle(),
       priority: this.getPriority(),
       completed: this.getIsCompleted()
+    }
+  }
+
+  #validateTitle(title) {
+    const isValid = this.#validator.isTitleCorrect(title)
+
+    if (isValid) {
+      return title
+    } else {
+      throw new Error('The title should be at most 50 characters long')
     }
   }
 }
