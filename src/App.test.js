57b4e66a5f75c9edc9384debfab3c12f0fa760bd
Tiberius Gherac <tiberius.gherac@gmail.com>
2025-12-02 10:08:27 +0100

    REFACTORING

--- src/App.test.js
+++ src/App.test.js
@@ -16,10 +16,12 @@ describe('App class test suite', () => {
     await app.start()
 
     // Assert
-    expect(outputMock.log).toHaveBeenCalledTimes(3)
-    expect(outputMock.log).toHaveBeenNthCalledWith(1, '1. List all saved tasks')
-    expect(outputMock.log).toHaveBeenNthCalledWith(2, '2. Add new task')
-    expect(outputMock.log).toHaveBeenNthCalledWith(3, '3. Exit')
+    expect(outputMock.log).toHaveBeenCalledTimes(5)
+    expect(outputMock.log).toHaveBeenNthCalledWith(1, '')
+    expect(outputMock.log).toHaveBeenNthCalledWith(2, '1. List all saved tasks')
+    expect(outputMock.log).toHaveBeenNthCalledWith(3, '2. Add new task')
+    expect(outputMock.log).toHaveBeenNthCalledWith(4, '3. Exit')
+    expect(outputMock.log).toHaveBeenNthCalledWith(5, '')
   })
 
   test('User should be able to input to choose option number 1', async () => {
